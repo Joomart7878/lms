8c01aaa36369f62685cd29ffd462e20dae2956de
Joomart Abdykulov <joomart.abd@gmail.com>
2025-09-30 13:21:59 +0600

    create packages and create user class

---
+++ src/main/java/com/joomart/lms/model/User.java
@@ -0,0 +1,52 @@
+package com.joomart.lms.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    // Stores hashed version of the password, not plain text
+    @Column(nullable = false)
+    private String passwordHash;
+
+    //Constructors, Getter, Setter
+    public User(String username, String passwordHash) {
+        this.username = username;
+        this.passwordHash = passwordHash;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    @Override
+    public String toString() {
+        return "User{id=" + id + ", username='" + username + "'}";
+    }
+}
