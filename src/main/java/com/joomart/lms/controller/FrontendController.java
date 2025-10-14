3e3e92e6a7c727bbc4d765e194b0ceae05fe5545
Joomart Abdykulov <joomart.abd@gmail.com>
2025-10-14 16:18:14 +0600

    Add implementation to FronendController

--- src/main/java/com/joomart/lms/controller/FrontendController.java
+++ src/main/java/com/joomart/lms/controller/FrontendController.java
@@ -1,4 +1,21 @@
 package com.joomart.lms.controller;
 
+import com.joomart.lms.data_transfer_objects.UserRegistrationDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class FrontendController {
+
+    @GetMapping("/")
+    public String index() {
+        return index();
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("userRegistrationDto", new UserRegistrationDto());
+        return "register";
+    }
 }
